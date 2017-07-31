6474600c7451643d0673dca5bdf7a4a41adf5c67
Sz. N. Bálint <szalaynb93@gmail.com>
2017-07-31 14:22:10 +0200

    hmm

--- bowling.py
+++ bowling.py
@@ -1,30 +1,29 @@
 def score(game):
     result = 0
     frame = 1
-    normal_game_frames = True
-    for i in range(len(game)):
-        if game[i] == '/':
+    SPARE = '/'
+    STRIKE = 'X'
+    in_first_half = True
+    for roll in range(len(game)):
+        if game[roll] == '/':
             result += 10 - last
         else:
-            result += get_value(game[i])
-        if frame < 10  and get_value(game[i]) == 10:
-            if game[i] == '/':
-                result += get_value(game[i+1])
-            elif game[i] == 'X' or game[i] == 'x':
-                result += get_value(game[i+1])
-                if game[i+2] == '/':
-                    result += 10 - get_value(game[i+1])
+            result += get_value(game[roll])
+        if frame < 10 and get_value(game[roll]) == 10:
+            if game[roll] == '/':
+                result += get_value(game[roll+1])
+            elif game[roll] == 'X' or game[roll] == 'x':
+                result += get_value(game[roll+1])
+                if game[roll+2] == '/':
+                    result += 10 - get_value(game[roll+1])
                 else:
-                    result += get_value(game[i+2])
-        last = get_value(game[i])
-        if not normal_game_frames:
+                    result += get_value(game[roll+2])
+        last = get_value(game[roll])
+        if not in_first_half:
             frame += 1
-        if normal_game_frames == True:
-            normal_game_frames = False
-        else:
-            normal_game_frames = True
-        if game[i] == 'X' or game[i] == 'x':
-            normal_game_frames = True
+        in_first_half = not in_first_half
+        if game[roll].upper() == 'X':
+            in_first_half = True
             frame += 1
     return result
 
